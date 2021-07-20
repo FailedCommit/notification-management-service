72cce8d96635115a54ecb6626bf5ea38107fd837
Mohit Kumar <mkumar@tekion.com>
2021-07-20 10:44:30 +0530

    P3M3: Configures health checking

--- src/main/java/com/manning/notification/manager/controllers/NotificationApi.java
+++ src/main/java/com/manning/notification/manager/controllers/NotificationApi.java
@@ -1,7 +1,5 @@
 package com.manning.notification.manager.controllers;
 
-import com.manning.notification.manager.model.NotificationGatewayRequest;
-import com.manning.notification.manager.model.NotificationGatewayResponse;
 import com.manning.notification.manager.model.NotificationRequest;
 import com.manning.notification.manager.model.NotificationResponse;
 import com.manning.notification.manager.services.NotificationService;
@@ -17,5 +15,10 @@ public class NotificationApi {
     @PostMapping
     public NotificationResponse sendNotification(@RequestBody NotificationRequest request) {
         return notificationService.sendNotification(request);
+    }
+
+    @GetMapping("/healthcheck")
+    public String healthCheck() {
+        return "UP";
     }
 }
